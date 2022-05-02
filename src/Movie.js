47b49aa6a97a0b37408a87d1266e71449f507e03
Vanibabu123 <vani87.babu@gmail.com>
2022-05-02 16:11:16 +0530

    My movie app created at again

--- src/Movie.js
+++ src/Movie.js
@@ -7,7 +7,7 @@ function Movie({ name, title, rating, summary }) {
      <div className='movie-container'>
          <img src={title} alt={name} className='movie-title' />
          <h2 className='movie-name'>{name}</h2>
-         <p className='movie-rating'>{rating}</p>
+         <p className='movie-rating'>/</p>
          <p className='movie-summary'>{summary}</p>
      </div>
  )
