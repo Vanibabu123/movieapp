91d00815ab973bb56eba425f2064831f8da66b0f
Vanibabu123 <vani87.babu@gmail.com>
2022-05-05 13:04:01 +0530

    My Add movie

--- src/Movie.js
+++ src/Movie.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 
 function Movie({ name, title, rating, summary }) {
-    
- return (
-     <div className='movie-container'>
-         <img src={title} alt={name} className='movie-title' />
-         <h2 className='movie-name'>{name}</h2>
-         <p className='movie-rating'>/</p>
-         <p className='movie-summary'>{summary}</p>
-     </div>
- )
+
+    return (
+        <div className='movie-container'>
+            <img src={title} alt={name} className='movie-title' />
+            <div className='movie-specs'>
+                <h2 className='movie-name'>{name}</h2>
+                <p className='movie-rating'>⭐ {rating}</p>
+            </div>
+            <p className='movie-summary'>{summary}</p>
+        </div>
+    )
 }
 
 export default Movie;
